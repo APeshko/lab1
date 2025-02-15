1e5fd5e359b9e6cd67c98cc1108ee81fbb848dd2
APeshko <m2415803@edu.misis.ru>
2025-02-15 10:58:15 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,9 @@
 int main() {
     int x = 10, y = 5;
     
-    std::cout << "Сложение: " << add(x, y) << std::endl;
-    std::cout << "Вычитание: " << subtract(x, y) << std::endl;
+    std::cout << add(x, y) << std::endl;
+    std::cout << subtract(x, y) << std::endl;
+    std::cout << multiply(x, y) << std::endl;
 
     return 0;
 }
