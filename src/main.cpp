f50e07df92eefe264cf12e739661ccd2f555c578
APeshko <m2415803@edu.misis.ru>
2025-02-15 10:39:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include <iostream>
+#include "utils.h"
 
+int main() {
+    int x = 10, y = 5;
+    
+    std::cout << "Сложение: " << add(x, y) << std::endl;
+    std::cout << "Вычитание: " << subtract(x, y) << std::endl;
+
+    return 0;
+}
